de86a8b8033439db3620e0229d99f7ca686cf401
Patric Lacouth <lacouth@gmail.com>
2022-11-29 08:00:38 -0300

    muda rota no post_test.py

--- post_test.py
+++ post_test.py
@@ -1,13 +1,15 @@
 import requests
 
-url = 'http://127.0.0.1:5000/post_data'
+# url = 'http://127.0.0.1:5000/post_data'
+url = 'https://tcc-laerte.onrender.com/post_data'
+
 headers = {'Accept' : 'application/json', 'Content-Type' : 'application/json'}
 
-data = {'sexo' : 'F',
-        'idade' : 22,
-        'RMSSD' : 100.0,
+data = {'sexo' : 'M',
+        'idade' : 30,
+        'RMSSD' : 170.0,
         'temperatura' : 20,
-        'umidade' : 90,
-        'luminancia' : 5000}
+        'umidade' : 50,
+        'luminancia' : 8000}
 
 r = requests.post(url, json=data, headers=headers)
